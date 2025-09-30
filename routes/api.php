98e1a8d6b865951a8d7114050a3c9fdfcc6fd7e7
phalla009 <hphalla3@gmail.com>
2025-09-30 10:49:59 +0700

    Edited api and dashboard

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\CategoryController;
+
 Route::get('products/kr/{id}', [ProductController::class, 'showById']);
 Route::apiResource('products/kr', ProductController::class);
 
